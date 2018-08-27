d5e7b78d508f95dfed26f18d877f43bc45065d8e
AndreasMuncier <olly_s@hotmail.co.uk>
2018-08-27 19:23:07 +0100

    Game of life

--- src/app1.c
+++ src/app1.c
@@ -52,29 +52,13 @@ static const u16 *g_ADC = 0;
 // buffer to store pad states for flash save
 #define BUTTON_COUNT 100
 
-// Two arrays of LED status data
 u8 state1[BUTTON_COUNT] = {0};
 u8 state2[BUTTON_COUNT] = {0};
 
-// Pointers for accessing the arrays
 u8 *currentState;
 u8 *nextState;
 
-// Define different states for program
-enum status_enum
-{
-	SET_RED = 0,
-	SET_GREEN = 1,
-	SET_BLUE = 2,
-	RUN = 3
-};
-
-// Default state
-u8 status = SET_RED;
-
-// Function declarations
-void setLED(u8 value, u8 index);
-u8 colourToMask(u8 colour);
+u8 start = 0;
 
 //______________________________________________________________________________
 
@@ -82,13 +66,12 @@ void app_surface_event(u8 type, u8 index, u8 value)
 {
 	if (type == TYPESETUP && value != 0)
 	{
-		status += 1;
-		status = status % 4;
+		start = 1;
 	}
-	if (type == TYPEPAD && value != 0 && status <= SET_BLUE)
+	if (type == TYPEPAD && value != 0 && !start)
 	{
-		currentState[index] = currentState[index] ^ colourToMask(status); // Toggle bit using XOR
-		setLED(currentState[index], index);
+		currentState[index] = 1 * !currentState[index];
+		hal_plot_led(TYPEPAD, index, 0, MAXLED * currentState[index], 0);	
 	}
 }
 
@@ -122,42 +105,30 @@ void app_cable_event(u8 type, u8 value)
 
 //______________________________________________________________________________
 
-u8 colourToMask(u8 colour)
+u8 countNeighbours(u8 index)
 {
-	return 1 << colour;
-}
+	s8 total = 0;
+	
+// 	total += currentState[index - 11];
+// 	total += currentState[index - 10];
+// 	total += currentState[index - 9];
+// 
+// 	total += currentState[index - 1];
+// 	total += currentState[index + 1];
+// 	
+// 	total += currentState[index + 9];
+// 	total += currentState[index + 10];
+// 	total += currentState[index + 11];	
 
-//______________________________________________________________________________
-
-u8 countNeighbours(u8 col, u8 row, u8 colour)
-{
-	u8 total = 0;
-	for (s8 i = -1; i < 2; ++i)
+	for (s8 i = -10; i < 20; i += 10)
 	{
 		for (s8 j = -1; j < 2; ++j)
 		{
-			u8 columnIndex = (10 + col + j) % 10;
-			u8 rowIndex = (10 + row + i) % 10;
-			total += (currentState[columnIndex + (10 * rowIndex)] & colourToMask(colour)) >> colour; // count neighbours
+			total += currentState[index + j + i];
 		}
 	}
-	total -= (currentState[col + (row * 10)] & colourToMask(colour)) >> colour; // remove count of own led
+	total -= currentState[index]; // remove count of own led
 	return total;
-}
-
-//______________________________________________________________________________
-
-void setLED(u8 value, u8 index)
-{
-	u8 r = 0, g = 0, b = 0;
-	if (value & colourToMask(SET_RED))
-		r = MAXLED;
-	if (value & colourToMask(SET_GREEN))
-		g = MAXLED;
-	if (value & colourToMask(SET_BLUE))
-		b = MAXLED;
-	
-	hal_plot_led(TYPEPAD, index, r, g, b);
 }
 
 //______________________________________________________________________________
@@ -166,7 +137,7 @@ void updateLEDS()
 {
 	for (u8 i = 0; i < BUTTON_COUNT; ++i)
     {
-    	setLED(currentState[i], i);
+		hal_plot_led(TYPEPAD, i, 0, currentState[i] * MAXLED, 0);
 	}
 }
 
@@ -174,48 +145,48 @@ void updateLEDS()
 
 void app_timer_event()
 {
-#define TICK_MS 100 // Refresh rate
-	
-    static u8 ms = TICK_MS;	
+    // example - send MIDI clock at 125bpm
+#define TICK_MS 200
+#define VIEW_REFRESH 100
     
-	if (++ms >= TICK_MS && status == RUN)
-	{
-		static u8 colour = SET_RED; // cycle through each colour
-		ms = 0;
-		
-		for (u8 row = 0; row < 10; ++row)
-		{
-			for (u8 col = 0; col < 10; ++col)
-			{
-				u8 index = col + (10 * row);				
-				u8 count = countNeighbours(col, row, colour);
+    if (start)
+    {
+   	    static u8 ms = 0;
+   	    
+   	    if (ms < VIEW_REFRESH)
+   	    	nextState[ms] = currentState[ms];
+   	    
+	    if (ms > 10 && ms < 89)
+    	{
+    		u8 count = countNeighbours(ms);
     		
-				if (count == 3)
-					nextState[index] = currentState[index] | colourToMask(colour);        			
-				else if (count == 2 && (currentState[index] & colourToMask(colour)))
-					nextState[index] = currentState[index] | colourToMask(colour);
-				else
-					nextState[index] = currentState[index] & ~colourToMask(colour);
-			}
-		}
-		
-		colour = (colour + 1) % 3;
-		
-		// Swap over arrays and update LEDs
-		u8 *temp;
-		temp = currentState;
-		currentState = nextState;
-		nextState = temp;
-		updateLEDS();
-	}
-
+	        if (count == 3)
+    	    	nextState[ms] = 1;        			
+	    	else if (count == 2 && currentState[ms] == 1)
+        		nextState[ms] = 1;
+	        else
+    	    	nextState[ms] = 0;
+	    }
+    	else if (ms >= VIEW_REFRESH)
+	    {
+    		hal_plot_led(TYPEPAD, ms - BUTTON_COUNT, 0, nextState[ms - BUTTON_COUNT] * MAXLED, 0);
+	    }
+	    if (ms >= TICK_MS)
+	    {
+	    	u8 *temp;
+	    	temp = currentState;
+    	    currentState = nextState;
+        	nextState = temp;
+	    	ms = -1;
+	    }
+		ms++;
+    }
 }
 
 //______________________________________________________________________________
 
 void app_init(const u16 *adc_raw)
 {
-	// Initialise pointers
 	currentState = state1;
 	nextState = state2;
 	// store off the raw ADC frame pointer for later use
